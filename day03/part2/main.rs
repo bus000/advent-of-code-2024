89e722768a90a6267e6a45910b01d9b75711ddd1
Magnus Stavngaard <magnus@stavngaard.dk>
2024-12-21 22:26:21 +0100

    Implement day 3 part 2

---
+++ day03/part2/main.rs
@@ -0,0 +1,310 @@
+// --- Part Two ---
+//
+// As you scan through the corrupted memory, you notice that some of the
+// conditional statements are also still intact. If you handle some of the
+// uncorrupted conditional statements in the program, you might be able to get
+// an even more accurate result.
+//
+// There are two new instructions you'll need to handle:
+//
+//    * The do() instruction enables future mul instructions.
+//    * The don't() instruction disables future mul instructions.
+//
+// Only the most recent do() or don't() instruction applies. At the beginning of
+// the program, mul instructions are enabled.
+//
+// For example:
+//
+//     xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+//
+// This corrupted memory is similar to the example from before, but this time
+// the mul(5,5) and mul(11,8) instructions are disabled because there is a
+// don't() instruction before them. The other mul instructions function
+// normally, including the one at the end that gets re-enabled by a do()
+// instruction.
+//
+// This time, the sum of the results is 48 (2*4 + 8*5).
+//
+// Handle the new instructions; what do you get if you add up all of the results
+// of just the enabled multiplications?
+use std::io;
+use std::io::Read;
+use std::process::ExitCode;
+use advent_of_code_2024::FiniteAutomata;
+use advent_of_code_2024::FiniteAutomataError;
+use advent_of_code_2024::StateRef;
+
+fn main() -> ExitCode {
+    let mut machine = Machine::build_machine();
+    let stdin = io::stdin();
+    let mut stdin_lock = stdin.lock();
+    let mut buffer = [0; 1024];
+
+    loop {
+        match stdin_lock.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(bytes_read) => {
+                for i in 0..bytes_read {
+                    machine.transition(buffer[i]);
+                }
+            }
+            Err(e) => {
+                eprintln!("Could not read stdin: {:?}", e);
+                return ExitCode::from(1);
+            }
+        }
+    }
+
+    println!("{:?}", machine.sum_of_muls);
+    return ExitCode::from(0);
+}
+
+/// State machine that will parse input and compute sum of all activated
+/// multiplications.
+struct Machine {
+
+    /// The current state of the parser.
+    parser_state: FiniteAutomata,
+
+    /// Reference of the state we end up in after finding an 'm'.
+    mul_end: StateRef,
+
+    /// Reference of the state we end up in after finishing parsing a mul()
+    /// expression.
+    mul_start: StateRef,
+
+    /// Reference of the state we are in while parsing 0-9 in first number of
+    /// mul expression.
+    n1_state: StateRef,
+
+    /// Reference of the state we are in while parsing 0-9 in second number of
+    /// mul expression.
+    n2_state: StateRef,
+
+    /// Reference of the state we end up in after finishing parsing a do()
+    /// expression.
+    do_end: StateRef,
+
+    /// Reference of the state we end up in after finishing parsing a dont()
+    /// expression.
+    dont_end: StateRef,
+
+    /// Whether or not the multiplication is currently active.
+    active: bool,
+
+    /// Running sum of active multiplications.
+    sum_of_muls: u32,
+
+    /// If parsing the first number of a multiplication it is buffered here.
+    n1: u32,
+
+    /// If parsing the second number of a multiplication it is buffered here.
+    n2: u32
+
+}
+
+impl Machine {
+
+    /// Build machine that can parse mul(), do(), and don't() functions.
+    fn build_machine() -> Machine {
+        Machine::build_machine_helper()
+            .expect("Unexpected machine building error.")
+    }
+
+    /// Build machine that can parse mul(), do(), and don't() functions.
+    fn build_machine_helper() -> Result<Machine, FiniteAutomataError> {
+        let mut automata = FiniteAutomata::new();
+        let initial = automata.current_state();
+        let mul_1 = automata.add_state();
+        let mul_2 = automata.add_state();
+        let mul_3 = automata.add_state();
+        let mul_4 = automata.add_state();
+        let mul_5 = automata.add_state();
+        let mul_6 = automata.add_state();
+
+        automata.add_transition(mul_1, &|c| c == b'u', mul_2)?;
+        automata.add_transition(mul_2, &|c| c == b'l', mul_3)?;
+        automata.add_transition(mul_3, &|c| c == b'(', mul_4)?;
+        automata.add_transition(mul_4, &|c| c >= b'0' && c <= b'9', mul_4)?;
+        automata.add_transition(mul_4, &|c| c == b',', mul_5)?;
+        automata.add_transition(mul_5, &|c| c >= b'0' && c <= b'9', mul_5)?;
+        automata.add_transition(mul_5, &|c| c == b')', mul_6)?;
+
+        let do_dont_1 = automata.add_state();
+        let do_dont_2 = automata.add_state();
+        let do_1 = automata.add_state();
+        let do_2 = automata.add_state();
+        let dont_1 = automata.add_state();
+        let dont_2 = automata.add_state();
+        let dont_3 = automata.add_state();
+        let dont_4 = automata.add_state();
+        let dont_5 = automata.add_state();
+
+        automata.add_transition(do_dont_1, &|c| c == b'o', do_dont_2)?;
+        automata.add_transition(do_dont_2, &|c| c == b'(', do_1)?;
+        automata.add_transition(do_dont_2, &|c| c == b'n', dont_1)?;
+        automata.add_transition(do_1, &|c| c == b')', do_2)?;
+        automata.add_transition(dont_1, &|c| c == b'\'', dont_2)?;
+        automata.add_transition(dont_2, &|c| c == b't', dont_3)?;
+        automata.add_transition(dont_3, &|c| c == b'(', dont_4)?;
+        automata.add_transition(dont_4, &|c| c == b')', dont_5)?;
+
+        // All states can start a new mut(), do() or don't().
+        automata.add_transition_all(&|c| c == b'm', mul_1)?;
+        automata.add_transition_all(&|c| c == b'd', do_dont_1)?;
+
+        // And all states can transition to the initial state on anything that
+        // doesn't match any other rule.
+        automata.add_transition_all(&|_| true, initial)?;
+
+        let machine = Machine {
+            parser_state: automata,
+            mul_end: mul_6,
+            mul_start: mul_1,
+            n1_state: mul_4,
+            n2_state: mul_5,
+            do_end: do_2,
+            dont_end: dont_5,
+
+            active: true,
+            sum_of_muls: 0,
+            n1: 0,
+            n2: 0,
+        };
+        return Ok(machine);
+    }
+
+    /// Transition the state of the machine based on the input byte.
+    fn transition(&mut self, byte: u8) {
+        self.parser_state.transition(byte)
+            .expect("All states have a match rule going to the initial state.");
+        let current_state = self.parser_state.current_state();
+        if current_state == self.mul_end && self.active {
+            self.sum_of_muls = self.sum_of_muls + self.n1 * self.n2;
+        } else if current_state == self.mul_start {
+            self.n1 = 0;
+            self.n2 = 0;
+        } else if current_state == self.do_end {
+            self.active = true;
+        } else if current_state == self.dont_end {
+            self.active = false;
+        } else if current_state == self.n1_state && byte != b'(' {
+            let n = (byte - b'0') as u32;
+            self.n1 = self.n1 * 10 + n;
+        } else if current_state == self.n2_state && byte != b',' {
+            let n = (byte - b'0') as u32;
+            self.n2 = self.n2 * 10 + n;
+        }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test that we can parse mul() expressions.
+    #[test]
+    pub fn test_build_machine_simple_mul() {
+        let mut machine = Machine::build_machine();
+
+        machine.transition(b'm');
+        machine.transition(b'u');
+        machine.transition(b'l');
+        machine.transition(b'(');
+        machine.transition(b'2');
+        machine.transition(b',');
+        machine.transition(b'5');
+        machine.transition(b'0');
+        machine.transition(b')');
+
+        assert_eq!(machine.sum_of_muls, 100);
+    }
+
+    /// Test that we can parse mul() expressions that doesn't begin right away.
+    #[test]
+    pub fn test_build_machine_mul_starting_different_place() {
+        let mut machine = Machine::build_machine();
+
+        machine.transition(b'm');
+        machine.transition(b'u');
+        machine.transition(b'l');
+        machine.transition(b'(');
+        machine.transition(b'g');
+        machine.transition(b'a');
+        machine.transition(b'r');
+        machine.transition(b'b');
+
+        machine.transition(b'm');
+        machine.transition(b'u');
+        machine.transition(b'l');
+        machine.transition(b'(');
+        machine.transition(b'2');
+        machine.transition(b',');
+        machine.transition(b'5');
+        machine.transition(b'0');
+        machine.transition(b')');
+
+        assert_eq!(machine.sum_of_muls, 100);
+    }
+
+    /// Test that we can parse do() and don't() expressions.
+    #[test]
+    pub fn test_build_machine_do_and_dont() {
+        let mut machine = Machine::build_machine();
+
+        machine.transition(b'm');
+        assert_eq!(machine.active, true);
+        machine.transition(b'u');
+        assert_eq!(machine.active, true);
+        machine.transition(b'l');
+        assert_eq!(machine.active, true);
+        machine.transition(b'(');
+        assert_eq!(machine.active, true);
+        machine.transition(b'd');
+        assert_eq!(machine.active, true);
+        machine.transition(b'o');
+        assert_eq!(machine.active, true);
+        machine.transition(b'n');
+        assert_eq!(machine.active, true);
+        machine.transition(b'\'');
+        assert_eq!(machine.active, true);
+        machine.transition(b't');
+        assert_eq!(machine.active, true);
+        machine.transition(b'(');
+        assert_eq!(machine.active, true);
+        machine.transition(b')');
+        assert_eq!(machine.active, false);
+        machine.transition(b'd');
+        assert_eq!(machine.active, false);
+        machine.transition(b'o');
+        assert_eq!(machine.active, false);
+        machine.transition(b'(');
+        assert_eq!(machine.active, false);
+        machine.transition(b'd');
+        assert_eq!(machine.active, false);
+        machine.transition(b'o');
+        assert_eq!(machine.active, false);
+        machine.transition(b'(');
+        assert_eq!(machine.active, false);
+        machine.transition(b')');
+        assert_eq!(machine.active, true);
+    }
+
+    /// Test that a larger example computes the correct sum.
+    #[test]
+    pub fn test_build_machine_large_example() {
+        let mut machine = Machine::build_machine();
+        let input = concat![
+            "asdfamul(mul(2,4)dont()mul(3,1)asdfdon't()mul(100,100)dafdodo(do(",
+            ")mul(7,7)asfd"
+        ];
+
+        for byte in input.bytes() {
+            machine.transition(byte);
+        }
+
+        assert_eq!(machine.sum_of_muls, 60);
+    }
+
+}
---
+++ lib/finite_automata.rs
@@ -0,0 +1,318 @@
+/// A reference to a state in the finite state machine.
+pub type StateRef = usize;
+
+/// A finite automata that can be used for string matching.
+///
+/// Contains a set of legal states with transitions on them. Contains a
+/// reference to the current state. A function FiniteAutomata.transition is used
+/// to transition between states.
+pub struct FiniteAutomata {
+
+    /// Reference of the current state.
+    current_state : StateRef,
+
+    /// All states.
+    states: Vec<State>,
+
+}
+
+impl FiniteAutomata {
+
+    /// Create a new finite automata.
+    ///
+    /// Will set the current_state as the given initial_state.
+    pub fn new() -> FiniteAutomata {
+        let mut states = Vec::new();
+        states.push(State::new(0));
+        return FiniteAutomata {
+            current_state: 0,
+            states: states
+        };
+    }
+
+    /// Get the current state of the finite automata.
+    pub fn current_state(&self) -> StateRef {
+        self.current_state
+    }
+
+    /// Get list of states in the finite automata.
+    pub fn states(&self) -> &Vec<State> {
+        &self.states
+    }
+
+    /// Add a state to a finite automata.
+    pub fn add_state(&mut self) -> StateRef {
+        let state_ref = self.states.len();
+        let state = State::new(state_ref);
+        self.states.push(state);
+        return state_ref;
+    }
+
+    /// Add a transition to a state.
+    ///
+    /// Will add a transition to state resulting_state_ref from state state_ref
+    /// if the predicate p matches a byte. The transition is added at the end of
+    /// the list of transitions on the state_ref state. If another transition is
+    /// matching before this one it will not be taken.
+    ///
+    /// # Errors
+    ///
+    /// Will give error FiniteAutomataError.MissingState if either state_ref or
+    /// resulting_state_ref could not be found on the finite automata.
+    pub fn add_transition(&mut self, state_ref : StateRef,
+        p: &'static dyn Fn(u8) -> bool, resulting_state_ref: StateRef)
+        -> Result<(), FiniteAutomataError> {
+
+        let state_count = self.states.len();
+        if state_ref >= state_count || resulting_state_ref >= state_count {
+            return Err(FiniteAutomataError::MissingState);
+        }
+        return match self.find_state_mut(state_ref) {
+            None => {
+                Err(FiniteAutomataError::MissingState)
+            },
+            Some(state) => {
+                state.add_transition(p, resulting_state_ref);
+                Ok(())
+            }
+        }
+    }
+
+    /// Add a transition to all states.
+    ///
+    /// Will add a transition to state resulting_state_ref from all state states
+    /// if the predicate p matches a byte. The transition is added at the end of
+    /// the list of transitions on the states. If another transition is matching
+    /// before this one it will not be taken.
+    ///
+    /// # Errors
+    ///
+    /// Will give error FiniteAutomataError.MissingState if resulting_state_ref
+    /// could not be found on the finite automata.
+    pub fn add_transition_all(&mut self, p: &'static dyn Fn(u8) -> bool,
+        resulting_state_ref: StateRef) -> Result<(), FiniteAutomataError> {
+
+        let state_count = self.states.len();
+        if resulting_state_ref >= state_count {
+            return Err(FiniteAutomataError::MissingState);
+        }
+        for state in self.states.iter_mut() {
+            state.add_transition(p, resulting_state_ref);
+        }
+
+        return Ok(())
+    }
+
+    /// Transition from the current state on the given byte.
+    ///
+    /// Will apply first matching transition to enter a new state.
+    ///
+    /// # Errors
+    ///
+    /// Will give error FiniteAutomataError.MissingTransition if no transition
+    /// matches the given byte.
+    pub fn transition(&mut self, byte: u8) -> Result<(), FiniteAutomataError> {
+        let state = self.states.get(self.current_state)
+            .expect("The current state can never refer to a missing state.");
+        for transition in state.transitions.iter() {
+            if (transition.predicate)(byte) {
+                self.current_state = transition.result_state_ref;
+                return Ok(());
+            }
+        }
+
+        return Err(FiniteAutomataError::MissingTransition);
+    }
+
+    /// Find the state with the given state_ref.
+    pub fn find_state(&self, state_ref: StateRef) -> Option<&State> {
+        return self.states.get(state_ref);
+    }
+
+    /// Find the state with the given state_ref.
+    fn find_state_mut(&mut self, state_ref: StateRef)
+        -> Option<&mut State> {
+
+        return self.states.get_mut(state_ref);
+    }
+
+}
+
+/// A state has a reference and a list of transitions to other states.
+pub struct State {
+
+    /// Unique name of the state.
+    state_ref: StateRef,
+
+    /// Transitions to other states.
+    transitions: Vec<Transition>
+
+}
+
+impl State {
+
+    /// Construct a new state with the given name.
+    pub fn new(state_ref: StateRef) -> State {
+        return State {
+            state_ref: state_ref,
+            transitions: Vec::new()
+        };
+    }
+
+    /// Get the unique ID of the state.
+    pub fn state_ref(&self) -> StateRef {
+        self.state_ref
+    }
+
+    /// Add another transition to this state.
+    ///
+    /// The transition is added to the end of the list of transitions and will
+    /// only be tried if all other transitions fail matching.
+    pub fn add_transition(&mut self, p: &'static dyn Fn(u8) -> bool,
+        result_state_ref: StateRef) {
+
+        let transition = Transition {
+            predicate: p,
+            result_state_ref: result_state_ref
+        };
+        self.transitions.push(transition);
+    }
+
+}
+
+/// A transition is a predicate matching a byte and a result state if matching.
+struct Transition {
+
+    /// Predicate matching whether the transition should be taken.
+    predicate : &'static dyn Fn(u8) -> bool,
+
+    /// The state to transition to on success.
+    result_state_ref : StateRef
+
+}
+
+/// Errors returned by finite automatas.
+#[derive(Debug, PartialEq)]
+pub enum FiniteAutomataError {
+
+    /// Error given when referencing a state that does not exist.
+    MissingState,
+
+    /// Error given when given a byte that doesn't match any transition in the
+    /// current state.
+    MissingTransition
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test that adding a state works as expected.
+    #[test]
+    fn test_add_state() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.add_state();
+        let state_2 = automata.add_state();
+        assert_eq!(0, automata.current_state);
+        assert_eq!(1, state_1);
+        assert_eq!(2, state_2);
+        assert_eq!(3, automata.states.len());
+    }
+
+    /// Test that adding state transitions works as expected.
+    #[test]
+    pub fn test_add_transition() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.current_state;
+        let state_2 = automata.add_state();
+        let predicate = &|c| c == 0x78;
+        automata.add_transition(state_1, predicate, state_2)
+            .expect("Both states exist, this should be fine.");
+
+        assert_eq!(0, automata.current_state);
+        assert_eq!(1, automata.states[0].transitions.len());
+        assert_eq!(0, automata.states[1].transitions.len());
+    }
+
+    /// Test that adding state transitions to states that doesn't exist will
+    /// give an error.
+    #[test]
+    pub fn test_add_transition_to_missing_state() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.current_state;
+        automata.add_state();
+        let predicate = &|c| c == 0x78;
+        let error = automata.add_transition(state_1, predicate, 44);
+
+        assert_eq!(Err(FiniteAutomataError::MissingState), error);
+    }
+
+    /// Test that adding state transitions from states that doesn't exist will
+    /// give an error.
+    #[test]
+    pub fn test_add_transition_from_missing_state() {
+        let mut automata = FiniteAutomata::new();
+        automata.current_state;
+        let state_2 = automata.add_state();
+        let predicate = &|c| c == 0x78;
+        let error = automata.add_transition(44, predicate, state_2);
+
+        assert_eq!(Err(FiniteAutomataError::MissingState), error);
+    }
+
+    /// Test that transitioning on a matching byte will switch state.
+    #[test]
+    pub fn test_transition() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.current_state;
+        let state_2 = automata.add_state();
+        let predicate = &|c| c == 0x78;
+        automata.add_transition(state_1, predicate, state_2)
+            .expect("Both states exist, this should be fine.");
+
+        assert_eq!(state_1, automata.current_state);
+        automata.transition(b'x').expect("We have a rule matching 'x'.");
+        assert_eq!(state_2, automata.current_state);
+    }
+
+    /// Test that transitioning on a non matching byte will error.
+    #[test]
+    pub fn test_transition_missing() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.current_state;
+        let state_2 = automata.add_state();
+        let predicate = &|c| c == 0x78;
+        automata.add_transition(state_1, predicate, state_2)
+            .expect("Both states exist, this should be fine.");
+
+        assert_eq!(state_1, automata.current_state);
+        let error = automata.transition(b'y');
+        assert_eq!(error, Err(FiniteAutomataError::MissingTransition));
+    }
+
+    /// Test that the first matching transition is used.
+    #[test]
+    pub fn test_transition_first_matching_rule_is_used() {
+        let mut automata = FiniteAutomata::new();
+        let state_1 = automata.current_state;
+        let state_2 = automata.add_state();
+        let state_3 = automata.add_state();
+
+        automata.add_transition(state_1, &|c| c == 0x78, state_2)
+            .expect("Both states exist, this should be fine.");
+        automata.add_transition(state_1, &|_| true, state_3)
+            .expect("Both states exist, this should be fine.");
+        automata.add_transition(state_2, &|_| true, state_1)
+            .expect("Both states exist, this should be fine.");
+
+        assert_eq!(state_1, automata.current_state);
+        automata.transition(b'x').expect("We have a rule matching 'x'.");
+        assert_eq!(state_2, automata.current_state);
+        automata.transition(b'x').expect("We have a rule matching 'x'.");
+        assert_eq!(state_1, automata.current_state);
+        automata.transition(b'y').expect("We have a rule matching 'y'.");
+        assert_eq!(state_3, automata.current_state);
+    }
+
+}
