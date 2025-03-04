06f9ff8a812c10043a3870cc78aae65145bd4fe9
Magnus Stavngaard <magnus@stavngaard.dk>
2025-03-04 15:28:35 +0100

    test

---
+++ tests/day-9.rs
@@ -0,0 +1,29 @@
+
+use std::fs;
+use assert_cmd::Command;
+
+/// Test that if we pass example input we will generate expected output.
+#[test]
+fn test_part_1_example_input() {
+    let input = "2333133121414131402\n";
+
+    Command::cargo_bin("day09_1")
+        .unwrap()
+        .write_stdin(input)
+        .assert()
+        .success()
+        .stdout(predicates::ord::eq("1928\n"));
+}
+
+/// Test that if we pass actual input we will compute expected output.
+#[test]
+fn test_part_1_actual_input() {
+    let input: String = fs::read_to_string("inputs/9.txt").unwrap();
+
+    Command::cargo_bin("day09_1")
+        .unwrap()
+        .write_stdin(input)
+        .assert()
+        .success()
+        .stdout(predicates::ord::eq("6332189866718\n"));
+}
